a5c64d18a4d30c66d2a691ff706dd335a1d4bddd
upranay <pranay.udapure@click2cloud.net>
2017-05-24 19:35:32 +0530

    root changed to welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  root 'welcome#index'
+  root 'welcome#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
